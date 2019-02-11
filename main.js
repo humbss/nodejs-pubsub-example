c015d808122fee58aa1972cc7a4e7046bf0e52d7
Thiago Humberto <Thiago HUmberto>
2019-02-11 13:09:35 -0200

    bulk

--- main.js
+++ main.js
@@ -1,20 +1,40 @@
 require('dotenv').config();
 
-const {PubSub} = require('@google-cloud/pubsub');
+const { PubSub } = require('@google-cloud/pubsub');
 const pubsub = new PubSub();
 
 // In this example, the message is current time
 const data = new Date().toString();
-const dataBuffer = Buffer.from('this is a message from client at: '+data);
-const topicName = 'orders';
 
-pubsub
-    .topic(topicName)
-    .publisher()
-    .publish(dataBuffer)
-    .then(messageId => {
-        console.log(`Message ${messageId} published.`);
-    })
-    .catch(err => {
-        console.error('ERROR:', err);
-    });
+const topicName = 'products';
+var noentries = process.argv[2];
+
+function sendBulk() {
+    for (i = 0; i < noentries; i++) {
+        var message = {
+            "id": i,
+            "insertDate": new Date(),
+            "productName": "Entry no: " + i,
+            "value": getRandomArbitrary(1,1000)
+        };
+
+        const dataBuffer = Buffer.from(JSON.stringify(message));
+
+        pubsub
+            .topic(topicName)
+            .publisher() 
+            .publish(dataBuffer)
+            .then(messageId => {
+                console.log(`Message ${messageId} published.`);
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+sendBulk();
+
